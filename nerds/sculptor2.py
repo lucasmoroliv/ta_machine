90763c5ff01ce3ab28d4e9309d8859ac76d357ac
Danilo Kenji Hayashi <danilokenjih@gmail.com>
2018-07-11 15:25:16 -0300

    sculptor2 updated and researcher2 made.

--- nerds/sculptor2.py
+++ nerds/sculptor2.py
@@ -2,13 +2,35 @@ import numpy as np
 import pandas as pd
 import time,calendar,datetime,csv,math,json
 from pprint import pprint
-import digger1k
+import digger2
 
 import matplotlib.pyplot as plt
 
 def callable(p,candles0_array):
     info_list = get_info_list(p,candles0_array,p['buy'])
     return info_list
+
+def get_info_list(p,candles0_array,buy):
+    df = get_dataframe(p['path_candle_file'])
+    td_s_array = td_array(p['path_td_file'],df)
+    candle_sec = df['timestamp'][1] - df['timestamp'][0]
+    df = df.set_index('timestamp')
+    df = update_df(df,'td',td_s_array[:,1])
+    candles_list = []
+    for row in candles0_array:
+        candles = {}
+        ts_candle0 = row
+        ts_buy = ts_candle0 + candle_sec*1
+        for index in range(p['buy']['candle'],p['sell']['candle']+1):
+            ts = ts_candle0 + candle_sec*index
+            candles[index] = {
+                'frombuytothishigh': ( df['high'][ts] - df['open'][ts_buy] ) / df['open'][ts_buy],
+                'frombuytothislow': ( df['low'][ts] - df['open'][ts_buy] ) / df['open'][ts_buy],
+            }
+        candles_list.append(candles)
+    p['sculptor']['keys'] = list(candles_list[0][list(candles_list[0])[0]].keys())
+    return candles_list
+
 
 def get_dataframe(file):
     return pd.read_csv(file, header=None, names=['time','timestamp','open','high','close','low','volume','change','amplitude'])
@@ -30,36 +52,6 @@ def update_df(df,name,array):
     serie = pd.Series(array)
     df[name] = array
     return df
-
-def get_info_list(p,candles0_array,buy):
-    df = get_dataframe(p['path_candle_file'])
-    td_s_array = td_array(p['path_td_file'],df)
-    candle_sec = df['timestamp'][1] - df['timestamp'][0]
-    candles_list = []
-    df = df.set_index('timestamp')
-    df = update_df(df,'td',td_s_array[:,1])
-    for row in candles0_array:
-        candles = {}
-        ts_candle0 = row
-        ts_buy_candle = ts_candle0 + candle_sec*buy['candle']
-        open_buy_candle = df['open'][ts_buy_candle]
-        volume_buy_candle = df['volume'][ts_buy_candle]
-        amplitude_buy_candle = df['amplitude'][ts_buy_candle]
-        for index in range(p['end_candle']+1):
-            ts = ts_candle0 + candle_sec*index
-            candles[index] = {
-                'ts' : ts,
-                'open': df['open'][ts],
-                'high': df['high'][ts],
-                'low': df['low'][ts],
-                'close': df['close'][ts],
-                'volume': df['volume'][ts],
-                'amplitude': df['amplitude'][ts],
-                'change': df['change'][ts],
-                'td_s': df['td'][ts],
-            }
-        candles_list.append(candles)
-    return candles_list
 
 if __name__ == '__main__':
     time1 = time.time()
