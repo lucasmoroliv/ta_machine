27ed9aa1a2304c715f47f2be9acb13959a66eb79
Lucas Moreira de Oliveira <lucasmoroliv@gmail.com>
2018-07-18 17:40:31 -0300

    Changes to bacon.py.

--- nerds/bacon.py
+++ nerds/bacon.py
@@ -9,30 +9,30 @@ class event:
         for key,value in p.items():
             setattr(self,key,value)
 
-        if '-' in self.buy['candle']:
-            self.buy['candle'] = self.buy['candle'].split('-')
-        if '-' in self.sell['candle']:
-            self.sell['candle'] = self.sell['candle'].split('-')
+        for act in ['sell','buy']:
+            if '-' in getattr(self,act)['candle']:
+                getattr(self,act)['candle'] = getattr(self,act)['candle'].split('-')
 
-        # '1h*1.05','2rsi*1.04','4c-500'.
-        for mode in ['*','+','-']:
-            if mode in self.buy['moment']:
-                part1,part2 = self.buy['moment'].split(mode)            
-                self.buy['moment'] = {}
-                self.buy['moment']['mode'] = mode
-                self.buy['moment']['candle'],self.buy['moment']['feature'] = self.split_num_str(str(part1))
-        # '5000','3500','2000'. Yet to think about this one. Letting it here for further investigation.
-        if isinstance(self.buy['moment'], numbers.Number):
-            self.buy['moment'] = {}
-            self.buy['moment']['mode'] = 'absolute'
-            self.buy['moment']['candle'] = 'any'
-            self.buy['moment']['feature'] = 'any'
-        # '1h','2rsi','4c'  
-        if sum(char.isdigit() for char in self.buy['moment']) == 1:
-            string = self.buy['moment']
-            self.buy['moment'] = {}
-            self.buy['moment']['candle'],self.buy['moment']['feature'] = self.split_num_str(string)
-            self.buy['moment']['mode'] = 'simple'
+            # '1h*1.05','2rsi*1.04','4c-500'.
+            for mode in ['*','+','-']:
+                if mode in getattr(self,act)['moment']: # self.buy['moment']
+                    part1,part2 = getattr(self,act)['moment'].split(mode) # self.buy['moment']            
+                    getattr(self,act)['moment'] = {} # self.buy['moment']
+                    getattr(self,act)['moment']['mode'] = mode # self.buy['moment']['mode']
+                    getattr(self,act)['moment']['candle'],getattr(self,act)['moment']['feature'] = self.split_num_str(str(part1))
+                    getattr(self,act)['moment']['change'] = part2 
+            # '5000','3500','2000'. Yet to think about this one. Letting it here for further investigation.
+            if isinstance(getattr(self,act)['moment'], numbers.Number):
+                getattr(self,act)['moment'] = {}
+                getattr(self,act)['moment']['mode'] = 'absolute'
+                getattr(self,act)['moment']['candle'] = 'any'
+                getattr(self,act)['moment']['feature'] = 'any'
+            # '1h','2rsi','4c'  
+            if sum(char.isdigit() for char in getattr(self,act)['moment']) == 1:
+                string = getattr(self,act)['moment']
+                getattr(self,act)['moment'] = {}
+                getattr(self,act)['moment']['candle'],getattr(self,act)['moment']['feature'] = self.split_num_str(string)
+                getattr(self,act)['moment']['mode'] = 'simple'
             
     def split_num_str(self,mess):
         x = np.array(list(mess))
@@ -44,6 +44,14 @@ class event:
         else:
             string_part = str(string_part[0])    
         return (str(number_part[0]),string_part)
+
+        
+
+
+
+
+
+
 
         # ope = {
         #     '+' : operator.add,
